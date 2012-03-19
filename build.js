4d8e4d890c123de17e5c5b1c3c00e072ad613e49
Jason Brown <loktar69@hotmail.com>
2012-03-18 23:00:49 -0500

    added build script, and unminified jest build

---
+++ build.js
@@ -0,0 +1,36 @@
+// settings
+var FILE_ENCODING = 'utf-8',
+    EOL = '\n',
+    DIST_FILE_PATH = 'build/jest.js';
+
+// setup
+var _fs = require('fs');
+
+function concat(fileList, distPath) {
+    var out = fileList.map(function(filePath){
+            return _fs.readFileSync(filePath, FILE_ENCODING);
+        });
+    _fs.writeFileSync(distPath, out.join(EOL), FILE_ENCODING);
+    console.log(' '+ distPath +' built.');
+}
+
+concat([
+    'js/jgame/vector.js',
+    'js/jgame/utilities.js',
+    'js/jgame/seedRandom.js',
+    'js/jgame/resource.js',
+    'js/jgame/resourceManager.js',
+    'js/jgame/label.js',
+    'js/jgame/game.js',
+    'js/jgame/renderer.js',
+    'js/jgame/sprite.js',
+    'js/jgame/emitter.js',
+    'js/jgame/particle.js',
+    'js/jgame/ui/ui.js',
+    'js/jgame/ui/button.js',
+    'js/jgame/ui/cursor.js',
+    'js/jgame/parralaxBackground.js',
+    'js/jgame/background.js',
+    'js/jgame/transition.js',
+    'js/jgame/background.js',
+], DIST_FILE_PATH);

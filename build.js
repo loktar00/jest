96a8df76e59fb954acbd2c0e2c1ed7e917a36914
Jason Brown <loktar69@hotmail.com>
2012-03-18 23:18:53 -0500

    Added minified build.

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 // settings
 var FILE_ENCODING = 'utf-8',
     EOL = '\n',
-    DIST_FILE_PATH = 'build/jest.js';
+    DIST_FILE_PATH = 'build/jest.js',
+	MIN_FILE = 'build/jest.min.js';
 
 // setup
 var _fs = require('fs');
@@ -10,7 +11,22 @@ function concat(fileList, distPath) {
     var out = fileList.map(function(filePath){
             return _fs.readFileSync(filePath, FILE_ENCODING);
         });
+		
     _fs.writeFileSync(distPath, out.join(EOL), FILE_ENCODING);
+    console.log(' '+ distPath +' built.');
+}
+
+function uglify(srcPath, distPath) {
+    var
+      uglyfyJS = require('uglify-js'),
+      jsp = uglyfyJS.parser,
+      pro = uglyfyJS.uglify,
+      ast = jsp.parse( _fs.readFileSync(srcPath, FILE_ENCODING) );
+
+    ast = pro.ast_mangle(ast);
+    ast = pro.ast_squeeze(ast);
+
+    _fs.writeFileSync(distPath, pro.gen_code(ast), FILE_ENCODING);
     console.log(' '+ distPath +' built.');
 }
 
@@ -34,3 +50,5 @@ concat([
     'js/jgame/transition.js',
     'js/jgame/background.js',
 ], DIST_FILE_PATH);
+
+uglify(DIST_FILE_PATH, MIN_FILE);
